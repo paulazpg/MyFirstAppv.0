6acb1f2dbd6a7a044ed167fb544103b3096a2c55
Paula - Georgiana ZALHAN <Paula.ZALHAN@office.ubbcluj.ro>
2020-10-06 21:27:48 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyFirstAppv._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Acesta e un test!");
+        }
+    }
+}
